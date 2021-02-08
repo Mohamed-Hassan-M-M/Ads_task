0ff04ca9d733902081328e835ac83aef3c9bd10f
Mohamed Hassan <75758174+Mohamed-Hassan-M-M@users.noreply.github.com>
2021-02-08 18:16:51 +0200

    Update message.blade.php

--- resources/views/dashboard/includes/alert/message.blade.php
+++ resources/views/dashboard/includes/alert/message.blade.php
@@ -1,5 +1,5 @@
 @if(Session::has('message'))
-    <div class="row mr-2 ml-2 mt-5">
+    <div class="row mr-2 ml-2">
         <alert type="text" class="btn btn-lg btn-block @if(Session::get('status') == 'success') btn-outline-success @else btn-outline-danger @endif mb-2"
                id="type-error">{{Session::get('message')}}
         </alert>
